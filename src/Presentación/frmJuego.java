e31f7529cb7ef0fe428c5624accf143c66bdd3bc
wmugiwara <wgu70804@gmail.com>
2020-07-05 12:01:44 -0600

    actualizacion canvas - cambiado por un jpanel problema flickering fixed

--- src/Presentación/frmJuego.java
+++ src/Presentación/frmJuego.java
@@ -7,7 +7,10 @@ package Presentación;
 
 import Util.GameObject;
 import Util.Render;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,33 +20,20 @@ import javax.swing.JOptionPane;
  *
  * @author Christopher Reyes
  */
-public class frmJuego extends javax.swing.JFrame {
+public class frmJuego extends javax.swing.JFrame implements Runnable{
 
     ArrayList<Render> ImageObjects;
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g); //To change body of generated methods, choose Tools | Templates.
-        for( int i = 0 ; i < ImageObjects.size() ;  i++){
-            g.drawImage( ImageObjects.get(i).getImage(), 0, 0, rootPane);
-        }
-    }
-    
-    private void initGame(){
-        
-            //System.out.println(getClass().getResource("../Files/horca.jpg").getFile());            
-            ImageObjects.add( new Render("horca.jpg"));
-        
-    }   
+ 
     
     /**
      * Creates new form frmJuego
-     */
-    
+     */    
     public frmJuego() {
-        ImageObjects = new ArrayList<>();
-        initComponents();
         
-        initGame();
+        initComponents();
+        ImageObjects = new ArrayList<>();   
+        start();
+        
     }
 
     /**
@@ -55,17 +45,66 @@ public class frmJuego extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        canvas1 = new java.awt.Canvas();
+        panelGame2 = new Util.PanelGame();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(153, 255, 153));
+        jPanel1.setMinimumSize(new java.awt.Dimension(300, 300));
+
+        canvas1.setBackground(new java.awt.Color(255, 255, 255));
+        canvas1.setMinimumSize(new java.awt.Dimension(600, 300));
+        canvas1.setPreferredSize(new java.awt.Dimension(600, 300));
+
+        javax.swing.GroupLayout panelGame2Layout = new javax.swing.GroupLayout(panelGame2);
+        panelGame2.setLayout(panelGame2Layout);
+        panelGame2Layout.setHorizontalGroup(
+            panelGame2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 366, Short.MAX_VALUE)
+        );
+        panelGame2Layout.setVerticalGroup(
+            panelGame2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 275, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(canvas1, javax.swing.GroupLayout.PREFERRED_SIZE, 391, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(61, 61, 61)
+                .addComponent(panelGame2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(161, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(canvas1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(43, 43, 43)
+                        .addComponent(panelGame2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(322, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 776, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 515, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -107,5 +146,59 @@ public class frmJuego extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private java.awt.Canvas canvas1;
+    private javax.swing.JPanel jPanel1;
+    private Util.PanelGame panelGame2;
     // End of variables declaration//GEN-END:variables
+
+    
+    @Override
+    public void run() {
+        int cont = 0;        while (true) {   
+           
+            if (cont <60) {
+                cont++;   
+                System.out.println("Ha pasado un segundo");                
+            }else{
+                cont=0;
+            }
+            this.update_canvas();
+            this.draw();
+            
+        }        
+    }
+    private Thread hilo;    
+    public void start(){
+        startComponents();        
+        hilo = new Thread(this);
+        hilo.start();       
+    }    
+    private void stop() throws InterruptedException{
+        JOptionPane.showMessageDialog(null, "El hilo a dejado de funcionar");
+        hilo.join();
+    }
+    private void startComponents(){  
+        ImageObjects.add( new Render("horca.jpg"));
+        ImageObjects.add( new Render("font.png"));
+            
+    }
+    private BufferStrategy bs;
+    private void draw(){
+        bs = canvas1.getBufferStrategy();
+        if (bs == null) {
+            canvas1.createBufferStrategy(3);
+            
+        }
+        
+        Graphics g = canvas1.getGraphics();
+        System.out.println(ImageObjects.size());
+        for( int i = 0; i < ImageObjects.size(); i++){
+            g.drawImage(ImageObjects.get(i).getImage() , 10, 10, null);
+        }
+        g.drawOval(20, 2, 2, 2);
+        
+    }
+    private void update_canvas(){
+        
+    }
 }
